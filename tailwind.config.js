28621040f03697e6b33ac494bdc360e83eab7f12
Rodrigo Vargas <rodrigovargas123@gmail.com>
2023-10-15 17:28:48 -0300

    Improvements on general layout. Added autoprefixer package

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    fontFamily: {
-      'sans': ['Poppins', 'sans-serif']
-    },
-    extend: {},
-  },
-  plugins: [],
+   content: [
+      "./src/**/*.{js,ts,jsx,tsx,mdx}",
+   ],
+   theme: {
+      container: {
+         center: true,
+         screens: {
+            sm: '640px',
+            md: '768px',
+         }
+      },
+      fontFamily: {
+         'sans': ['Poppins', 'sans-serif']
+      },
+      extend: {},
+   },
+   plugins: [],
 }
 
