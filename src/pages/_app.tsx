14d5b51964484acf013f0d67333408f504b66218
rodrigo-vargas <rodrigovargas123@gmail.com>
2023-12-14 18:23:55 -0300

    Added canonical URL to site pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import "../main.css";
 import 'highlight.js/styles/github-dark.css';
 import GoogleTagManager from 'src/components/GoogleTagManager';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 
 const poppins = Poppins({
   weight: ['100', '200', '300', '600', '700'],
@@ -11,10 +12,14 @@ const poppins = Poppins({
 });
  
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const canonicalUrl = (`https://www.rodrigovargas.com.br` + (router.asPath === "/" ? "": router.asPath)).split("?")[0];
+
   return (
     <>
       <Head>
         <title>Rodrigo Vargas</title>
+        <link rel="canonical" href={canonicalUrl} />
       </Head>
       <main className={poppins.className}>
         <Component {...pageProps} />
